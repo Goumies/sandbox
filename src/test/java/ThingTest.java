419b0b2e88d3ac4e09f328f44251d323b3a333a9
Romy <contact@romyalula.com>
2019-01-25 19:38:08 +0100

    Configure sandbox project

---
+++ src/test/java/ThingTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ThingTest {
+    @Test
+    public void name() {
+        assertThat(0).isEqualTo(0);
+    }
+}
